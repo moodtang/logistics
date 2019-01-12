932990addf25e93faf70db396e9a2daf60cb5f41
lenovo <1783742430@qq.com>
2019-01-12 22:49:19 +0800

    获取用户列表，删除用户

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,11 +4,15 @@ import com.example.demo.annotation.UserLoginToken;
 import com.example.demo.entity.UserEntity;
 import com.example.demo.service.TokenService;
 import com.example.demo.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by computer on 2019/1/8.
@@ -21,17 +25,28 @@ public class UserController {
     UserService userService;
     @Autowired
     TokenService tokenService;
+    /*
+    * 测试
+    * */
+    @ApiOperation(value = "")
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String HelloWorld() {
         return "Hello World!";
     }
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String addUser(UserEntity userEntity) {
-        return userService.addUser(userEntity);
+    /*
+    *  注册
+    * */
+    @ApiOperation(value = "注册")
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public HashMap addUser(UserEntity userEntity) {
+        HashMap result = new HashMap();
+        result.put("uid",userService.addUser(userEntity));
+        return  result;
     }
     //登录
+    @ApiOperation(value = "登录")
     @PostMapping("/login")
-    public HashMap login(UserEntity user) throws JSONException {
+    public HashMap login(UserEntity user) {
         UserEntity userForBase=userService.findByUsername(user);
         HashMap tokenUser = new HashMap();
         if(userForBase==null){
@@ -49,6 +64,34 @@ public class UserController {
             }
         }
     }
+    /*
+     *删除用户
+     * */
+//    @UserLoginToken
+    @ApiOperation(value = "")
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public HashMap deleteUser(@RequestParam("username")String username) {
+        HashMap result = new HashMap();
+        result.put("msg",userService.deleteUser(username));
+        return result;
+    }
+    /*
+     *获取用户列表
+     * */
+//    @UserLoginToken
+    @ApiOperation(value = "用户列表")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public HashMap list() {
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        List<UserEntity> list = new ArrayList<>();
+        list = userService.getUserList();
+        result.put("userList",list);
+        return result;
+    }
+
+    /*
+    * 带token值测试
+    * */
     @UserLoginToken
     @GetMapping("/getMessage")
     public String getMessage(){
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -6,6 +6,9 @@ import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 /**
  * Created by computer on 2019/1/9.
  */
@@ -14,6 +17,7 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
     /*注册用户*/
+    @Transactional
     public String addUser(UserEntity userEntity){
         //      设置uid
         GetUid getUid =new GetUid();
@@ -25,16 +29,39 @@ public class UserService {
         if(userRepository.findUserEntityByUserPhone(userEntity.getUserPhone())!=null){
             return "the phone have exited ";
         }
-        else
-            userRepository.save(userEntity);
-            return uid;
+        else{
+            if(userEntity.getUsername() != null && userEntity.getPassword() != null && userEntity.getUserPhone() != null) {
+                userRepository.save(userEntity);
+                return uid;
+            }
+        }
+            return "register fail";
+
     }
     /*登录*/
     public UserEntity login(String username){
        return userRepository.findUserEntityByUsername(username);
 
     }
+    /*
+    * 删除用户
+    * */
+    @Transactional
+    public String deleteUser(String username){
+        if (userRepository.deleteUserEntityByUsername(username)>0)
+            return "删除用户成功";
+        else
+            return "删除用户失败";
+    }
+    /*通过用户名查找userEntity
+    * */
     public UserEntity findByUsername(UserEntity userEntity){
        return userRepository.findUserEntityByUsername(userEntity.getUsername());
+    }
+    /*
+    * 获取用户列表
+    * */
+    public List<UserEntity> getUserList(){
+        return userRepository.getUserList();
     }
 }
